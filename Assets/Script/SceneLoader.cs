af00b693b9601bf39c822db5001eff95deb27319
AlexGearX <alexgear34@gmail.com>
2020-06-14 19:55:00 +0200

    update

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -8,5 +8,14 @@ public class SceneLoader : MonoBehaviour
     public void LoadNextScene()
     {
         int curentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(curentSceneIndex + 1);
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
